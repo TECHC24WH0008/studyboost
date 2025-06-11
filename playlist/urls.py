ac9ef2ddfa97372dd03259feb27be50c9f91e21b
Tasisei23 <ttc2450sw0008@edu.tech.ac.jp>
2025-06-11 11:09:34 +0900

    Delete playlist directory

--- playlist/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'playlist'
-
-urlpatterns = [
-    path('', views.playlist_detail, name='playlist_detail'),      # 1つの再生リスト詳細画面
-    path('add-video/', views.add_video, name='add_video'),        # 動画追加
-]
